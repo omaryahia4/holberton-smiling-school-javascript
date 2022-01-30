151bbfcb8bdc793ab201ffcfdc9dbf9dbf35c805
omaryahia4 <omar.yahia4444@gmail.com>
2022-01-30 12:42:16 +0100

    task 4 and 5

--- scripts.js
+++ scripts.js
@@ -25,6 +25,7 @@ $(document).ready(function() {
                 `);
                 
                 $('#carousel-inner').append(html);
+                $('#carousel-inner1').append(html);
             }
         }
     }
@@ -74,6 +75,54 @@ $(document).ready(function() {
                 </div>
                 `);
             $('#tutorial-cards-container').append(html1);
+            }
+        }
+    })
+    $.ajax({
+        url: 'https://smileschool-api.hbtn.info/latest-videos',
+        method: 'GET',
+        dataType: 'json',
+        beforeSend: function(){
+            $('div.loader').show();
+        },
+        success: function(data) {
+            $('div.loader').hide();
+            for (let i = 0; i < data.length; i++) {
+                let html2 = (`
+                <div class="text-center col-12 col-sm-6 col-md-3">
+                    <div class="carousel-item active">
+                        <div class='tutorial-video-top'>
+                            <img class="w-100" src=${data[i].thumb_url} alt="smile image">
+                            <img src="./images/play.png" width="80" height="80" style="position: absolute; top: 30px; right: 80px;">
+                        </div>
+                        <div class="mx-1">
+                            <div class="font-weight-bold text-dark text-left mt-3">
+                                ${data[i].title}
+                            </div>
+                            <div class="text-secondary text-left mt-3 mb-3">
+                                ${data[i]['sub-title']}
+                            </div>
+                            <div class="d-flex align-items-center mb-3">
+                                <img src=${data[i].author_pic_url} class="rounded-circle" width="30" height="30" alt="profile image">
+                                <div class="ml-2" style="color: #C271FF;">${data[i].author}</div>
+                            </div>
+                            <div class="d-flex justify-content-between">
+                                <div class="d-flex pt-1">
+                                    <img class="mr-1" src="./images/star_on.png" width="15" height="15">
+                                    <img class="mr-1" src="./images/star_on.png" width="15" height="15">
+                                    <img class="mr-1" src="./images/star_on.png" width="15" height="15">
+                                    <img class="mr-1" src="./images/star_on.png" width="15" height="15">
+                                    <img src="./images/star_off.png" width="15" height="15">
+                                </div>
+                                <div class="ml-5" style="color: #C271FF;">
+                                    ${data[i].duration}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                `);
+            $('#latest-videos-cards').append(html2);
             }
         }
     })
