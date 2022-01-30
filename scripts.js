e21d754149fd47456741d458a8ea8606c8cd9bf2
omaryahia4 <omar.yahia4444@gmail.com>
2022-01-30 10:39:24 +0100

    Homepage - quotes - API

--- scripts.js
+++ scripts.js
@@ -11,9 +11,11 @@ $(document).ready(function() {
             $('div.loader').hide();
             for (let i = 0; i < data.length; i++) {
                 let html = (`   
-                    <div class="carousel-item ${i == 0 ? 'active' : ''} text-white d-flex justify-content-center p-5 id="carousel-item">
-                        <img class="rounded-circle " src=${data[i].pic_url} alt="First slide" width="120" height="120"></img>
-                        <div class="w-50 ml-4">
+                    <div class="carousel-item  ${i == 0 ? 'active' : ''} text-white p-5 id="carousel-item">
+                        <div class="quote-image">
+                            <img class="rounded-circle text-center" src=${data[i].pic_url} alt="First slide" width="120" height="120"></img>
+                        </div>
+                        <div class="quote w-50 ">
                             <p>${data[i].text}</p>
                             <h6 class="font-weight-bold">${data[i].name}</h6>
                             <p class="font-italic">${data[i].title}</p>
@@ -28,4 +30,18 @@ $(document).ready(function() {
     }
 
     )
+    $.ajax({
+        url: 'https://smileschool-api.hbtn.info/popular-tutorials',
+        method: 'GET',
+        dataType: 'json',
+        beforeSend: function(){
+            $('div.loader').show();
+        },
+        success: function(data) {
+            $('div.loader').hide();
+            for (let i = 0; i < data.length; i++) {
+                let html1 = (``)
+            }
+        }
+    })
 })
